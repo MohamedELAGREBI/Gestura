eb85359f408b85a6aad0c6b2e6d20fe8737aa43c
Mohamed EL AGREBI <freelance.daedalus@gmail.com>
2024-09-18 20:05:54 +0200

    Génération du projet .net MAUI

---
+++ Gestura/Gestura/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gestura
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Gestura/Gestura/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestura
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
